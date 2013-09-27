480c34c080f2490c959505a1f4e317aca7c02ab1
Lixfe <felix.leo.lecuyer@gmail.com>
2013-09-27 17:12:48 +0200

    Création de la classe ImagePGM
    Création du constructeur par défaut

---
+++ src/tp_eclipse/ImagePGM.java
@@ -0,0 +1,22 @@
+package tp_eclipse;
+
+public class ImagePGM {
+int largeur;
+int longueur;
+int maxNiveauGris;
+int pixels[][];
+
+/**
+ * Constructeur par défaut de la classe ImagePGM
+ * 
+ *Initialise une image vide.
+ */
+public ImagePGM(){
+	this.largeur=0;
+	this.longueur=0;
+	this.maxNiveauGris = 0;
+	this.pixels = new int[0][0];
+}
+
+
+}
