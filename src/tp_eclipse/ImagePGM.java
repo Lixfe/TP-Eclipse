f80afcbb3ee45d131669f3edcae1798db2b90767
Lixfe <felix.leo.lecuyer@gmail.com>
2013-09-29 15:04:06 +0200

    Creation du constructeur a partir d'un fichier source

--- src/tp_eclipse/ImagePGM.java
+++ src/tp_eclipse/ImagePGM.java
@@ -1,15 +1,13 @@
 package tp_eclipse;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 
 public class ImagePGM {
 	
 
 private	int largeur;
-private int longueur;
+private int hauteur;
 private int maxNiveauGris;
 private int pixels[][];
 
@@ -21,10 +19,62 @@ private int pixels[][];
  */
 public ImagePGM(){
 	this.largeur=0;
-	this.longueur=0;
+	this.hauteur=0;
 	this.maxNiveauGris = 0;
 	this.pixels = new int[0][0];
 }
+
+/**
+ * Constructeur d'initialisation à partir d'un fichier image PGM source
+ * @param nomFichier
+ */
+public ImagePGM(String nomFichier){
+
+	File f = new File(nomFichier);
+	Scanner sc;
+	
+	try {
+		sc = new Scanner(f);
+
+	
+	
+	String a = sc.nextLine();
+	
+	a = sc.nextLine();
+	
+	
+	this.largeur = sc.nextInt();
+	this.hauteur = sc.nextInt();
+	
+	
+	a = sc.nextLine();
+	
+	this.maxNiveauGris = sc.nextInt();
+	
+	a = sc.nextLine();
+	
+	this.pixels = new int[this.hauteur][this.largeur];
+	
+	for (int i = 0; i < this.hauteur; i++) {
+		for (int j = 0; j < this.largeur; j++) {
+			this.pixels[i][j] = sc.nextInt();
+		}
+		
+		a = sc.nextLine();
+	}
+	
+	sc.close();
+	
+	} 
+	
+	catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		System.out.println("File Not Found");
+	}
+	
+}
+
+
 
 
 }
--- src/tp_eclipse/projetPGM.java
+++ src/tp_eclipse/projetPGM.java
@@ -1,10 +1,12 @@
 package tp_eclipse;
+import java.io.*;
 
 public class projetPGM {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
-
+		ImagePGM imageIena = new ImagePGM("lena.pgm");
+}
+	
 }
